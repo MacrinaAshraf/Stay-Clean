67c9d95eda3a8dd1b47c30d8eaf80aa60fb4a2ea
MacrinaAshraf <macrina1997@gmail.com>
2020-06-24 20:48:16 +0200

    add view button to program card and add sort by price to list program from user view

--- frontend/src/views/programs/ListAllPrograms.js
+++ frontend/src/views/programs/ListAllPrograms.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProgramCard from "./utils/ProgramCard.js";
 
-import { Container, Row, Col, Input } from "reactstrap";
+import { Container, Row, Col, Input, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 
 // core components
 import Hero from "../IndexSections/Hero.js";
@@ -14,6 +14,10 @@ const List = () => {
 
     const [programs, setPrograms] = useState([])
     const [filtered, setFiltered] = useState([]);
+    const [isOpen, setOpen] = useState(false);
+
+    const toggle = () => setOpen(!isOpen);
+
     useEffect(() => {
         axios.get("http://127.0.0.1:8000/api/programs/", {
             headers: {
@@ -27,18 +31,50 @@ const List = () => {
 
     }, []);
 
+
+    const handleClick = (e) => {
+        console.log(e.target.value);
+
+        if (e.target.value === "lowest") {
+            const compareLowest = (a, b) => {
+                if (parseInt(a.price) < parseInt(b.price)) {
+                    return -1;
+                }
+                if (parseInt(a.price) > parseInt(b.price)) {
+                    return 1;
+                }
+            }
+            let list = filtered;
+            list.sort(compareLowest);
+            setFiltered(list);
+        }
+        else if (e.target.value === "highest") {
+            const compareHighest = (a, b) => {
+                if (parseInt(a.price) > parseInt(b.price)) {
+                    return -1;
+                }
+                if (parseInt(a.price) < parseInt(b.price)) {
+                    return 1;
+                }
+            }
+            let list = filtered;
+            list.sort(compareHighest);
+            setFiltered(list);
+        }
+    }
+
     const handleChange = (e) => {
 
         let currentList = [];
         let newList = [];
         console.log(e.target.value)
-        
+
         if (e.target.value !== "") {
             currentList = programs;
 
             newList = currentList.filter(item => {
                 const lc = item.name.toLowerCase();
-                if(lc.includes(e.target.value.toLowerCase()))
+                if (lc.includes(e.target.value.toLowerCase()))
                     return item;
             });
         } else {
@@ -55,18 +91,28 @@ const List = () => {
                     <Hero />
                     <section className="section section-lg pt-lg-0 mt--200">
                         <Container>
-                            <Input
-                                type="text"
-                                className="input"
-                                onChange={handleChange}
-                                placeholder="Search..."
-                            />
-                            {/* <SearchBar options={this.state.programs} /> */}
+                            <div>
+                                <Input
+                                    type="text"
+                                    className="input"
+                                    onChange={handleChange}
+                                    placeholder="Search..."
+                                />
+                                <br />
+                                <ButtonDropdown isOpen={isOpen} toggle={toggle}>
+                                    <DropdownToggle color="secondary" caret size="sm">
+                                        Sort by price
+                                </DropdownToggle>
+                                    <DropdownMenu>
+                                        <DropdownItem value="lowest" onClick={handleClick}>Lowest</DropdownItem>
+                                        <DropdownItem value="highest" onClick={handleClick}>Highest</DropdownItem>
+                                    </DropdownMenu>
+                                </ButtonDropdown>
+                            </div>
                             <br />
                             <Row className="justify-content-center">
                                 <Col lg="12">
                                     <Row className="row-grid">
-                                        {/* {console.log()} */}
                                         {filtered.map((item) => (
                                             <ProgramCard program={item} key={item.id} />
                                         ))}
--- frontend/src/views/programs/utils/ProgramCard.js
+++ frontend/src/views/programs/utils/ProgramCard.js
@@ -9,35 +9,42 @@ const ProgramCard = (props) => {
   return (
     <Col lg="4">
       {/* <Link to={'/programs/' + props.program.id}> */}
-        <Card className="card-lift--hover shadow border-0">
-          <CardBody className="py-5">
-            <div className="icon icon-shape icon-shape-info rounded-circle mb-4">
-              <i className="ni ni-istanbul" />
-            </div>
+      <Card className="card-lift--hover shadow border-0">
+        <CardBody className="py-5">
+          <div className="icon icon-shape icon-shape-info rounded-circle mb-4">
+            <i className="ni ni-istanbul" />
+          </div>
 
-            <h6 className="text-info text-uppercase">{props.program.name}</h6>
+          <h6 className="text-info text-uppercase">{props.program.name}</h6>
 
-            <p className="description mt-3">{props.program.description}</p>
-            <div>
-              <Badge color="info" pill className="mr-1  ">
-                {props.program.price} $
+          <p className="description mt-3">{props.program.description}</p>
+          <div>
+            <Badge color="info" pill className="mr-1  ">
+              {props.program.price} $
             </Badge>
-              <Badge color="info" pill className="mr-1">
-                {props.program.duration} days
+            <Badge color="info" pill className="mr-1">
+              {props.program.duration} days
             </Badge>
-            </div>
+          </div>
 
-            {props.children}
-            <div style={{ marginTop: 10 }}>
-              <StarRatingComponent
-                name="avgRate"
-                starCount={5}
-                value={parseInt(props.program.avgRate)}
-                editing={false}
-              />
-            </div>
-          </CardBody>
-        </Card>
+          {props.children}
+          <div style={{ marginTop: 10 }}>
+            <StarRatingComponent
+              name="avgRate"
+              starCount={5}
+              value={parseInt(props.program.avgRate)}
+              editing={false}
+            />
+          </div>
+          <Link 
+            color="info"
+            className="mt-4 btn-outline-info btn"
+            to={'/programs/' + props.program.id}
+          >
+            view
+          </Link>
+        </CardBody>
+      </Card>
       {/* </Link> */}
     </Col>
   );
